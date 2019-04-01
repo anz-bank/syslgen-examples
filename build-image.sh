e9410025eb1596c480aac09c92c0543144fce1a9
Kasun <Kasun.Fernando@anz.com>
2019-04-02 10:15:15 +1100

    Modify secure variables, refactor build script

--- build-image.sh
+++ build-image.sh
@@ -3,10 +3,28 @@
 set -e -x
 
 TAG=$1
+COMMAND=$2
+
+if [ "$COMMAND" != "build" ] && [ "$COMMAND" != "deploy" ]; then
+    echo "Usage: build-image.sh [tag] [build|deploy]"
+    exit 1
+fi
 
 SYSL2_VERSION=0.2.5-ubuntu18.04
-DOCKER_IMAGE_LATEST="anzbank/syslgen-examples"
-DOCKER_IMAGE_VERSIONED="$DOCKER_IMAGE_LATEST:${TAG}"
+DOCKER_IMAGE="anzbank/syslgen-examples"
+DOCKER_IMAGE_VERSIONED="$DOCKER_IMAGE:${TAG}"
 
-docker build --build-arg SYSL2_VERSION=$SYSL2_VERSION -t "$DOCKER_IMAGE_LATEST" -f Dockerfile .
-docker tag "$DOCKER_IMAGE_LATEST" "$DOCKER_IMAGE_VERSIONED"
+build() {
+    docker build --build-arg SYSL2_VERSION=$SYSL2_VERSION -t "$DOCKER_IMAGE" -f Dockerfile .
+    docker tag "$DOCKER_IMAGE" "$DOCKER_IMAGE_VERSIONED"
+}
+
+deploy() {
+    docker push "$DOCKER_IMAGE_VERSIONED" && docker push "$DOCKER_IMAGE"
+}
+
+if [ "$COMMAND" = "build" ]; then
+    build
+elif [ "$COMMAND" = "deploy" ]; then
+    deploy
+fi
