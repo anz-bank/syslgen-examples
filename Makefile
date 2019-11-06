1d3d8601f27157d21341733bdde51a7e8dba749b
Jack Zheng <jack.zheng@anz.com>
2019-11-06 14:46:41 +1100

    Add CLI options to client

--- Makefile
+++ Makefile
@@ -1,39 +1,25 @@
 .PHONY: all
-all: clean/bin generate/todos gotools/todos compile
+all: clean compile
 
-.PHONY: clean
-clean: clean/bin
-
-.PHONY: gotools
-gotools: gotools/todos
-
+# Generate doesn't run automatically on make all. It can be run manually via make generate
 .PHONY: generate
 generate: generate/todos
-
-.PHONY: validate
-validate: validate/todos
 
 .PHONY: compile
 compile: todos_client todos_server
 
-clean/%:
-	-rm $*_client/$*-client
-	-rm $*_server/$*-server
+.PHONY: clean
+clean:
+	-rm bin/client
+	-rm bin/server
 
+.PHONY: todos_client
 todos_client:
-	cd cmd/todos_client; go build -o ../../bin/client *.go; cd -
+	go build -o ./bin/client cmd/todos_client/*.go
 
+.PHONY: todos_server
 todos_server:
-	cd cmd/todos_server; go build -o ../../bin/server *.go; cd -
-
-mkdir/%:
-	echo "creating output dir" $*
-	-mkdir -p $*
-
-gotools/%:
-	gofmt -w $*/
-	goimports -w $*/
-	golangci-lint run $*
+	go build -o ./bin/server cmd/todos_server/*.go
 
 generate/%:
 	cd codegen && ./generate.sh
--- cmd/todos_client/main.go
+++ cmd/todos_client/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/anz-bank/syslgen-examples/gen/todos"
 	"net/http"
 	"net/http/httptrace"
+
+	"github.com/anz-bank/syslgen-examples/gen/todos"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	todo   = kingpin.Command("todo", "Get todos from server.")
+	todoID = todo.Arg("id", "Todo ID").Required().Int64()
+
+	posts = kingpin.Command("posts", "Get posts from server.")
+
+	comment      = kingpin.Command("comment", "Create comments on server.")
+	commentTitle = comment.Arg("title", "Comment Title").Required().String()
+	commentBody  = comment.Arg("body", "Comment Content").Required().String()
 )
 
 func withTrace(ctx context.Context) context.Context {
@@ -20,15 +34,46 @@ func withTrace(ctx context.Context) context.Context {
 	return httptrace.WithClientTrace(ctx, trace)
 }
 
+func prettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
+
 func main() {
 	httpClient := http.Client{}
 	client := todos.NewClient(&httpClient, "http://localhost:8080")
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	result, err := client.GetTodosID(withTrace(ctx), map[string]string{}, 1)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err.Error())
-	} else {
-		fmt.Printf("Status: %d\nValue: \n%+v\n", result.HTTPResponse.StatusCode, result.Response)
+
+	switch kingpin.Parse() {
+	case "todo":
+		result, err := client.GetTodosID(withTrace(ctx), map[string]string{}, *todoID)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+		} else {
+			fmt.Printf("Status: %d\nValue: \n%+v\n", result.HTTPResponse.StatusCode, prettyPrint(result.Response))
+		}
+	case "posts":
+		// List all posts
+		result, err := client.GetPosts(withTrace(ctx), map[string]string{})
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+		} else {
+			fmt.Printf("Status: %d\nValue: \n%+v\n", result.HTTPResponse.StatusCode, prettyPrint(result.Response))
+		}
+	case "comment":
+		post := todos.Post{
+			ID:     1,
+			Body:   *commentBody,
+			Title:  *commentTitle,
+			UserID: 4,
+		}
+		result, err := client.PostComments(withTrace(ctx), map[string]string{}, &post)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+		} else {
+			fmt.Printf("Status: %d\nValue: \n%+v\n", result.HTTPResponse.StatusCode, prettyPrint(result.Response))
+		}
 	}
+
 }
